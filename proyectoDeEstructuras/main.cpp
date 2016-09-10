4a956f048dc86e83a0df6fa9d6b1a11a5909049c
Jafeth  Vásquez <jvsquez19@gmail.com>
2016-09-10 13:55:30 -0600

    Initial Commit

---
+++ proyectoDeEstructuras/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  proyectoDeEstructuras
+//
+//  Created by Jafeth  Vásquez on 10/9/16.
+//  Copyright © 2016 Jafeth Vásquez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
